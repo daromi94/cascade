a5bca87a089ae51a8b28921b3d0299e9e2274075
daromi94 <davromero07@gmail.com>
2025-01-30 20:18:15 -0500

    perf: :zap: handle small prime cases

--- src/core/src/main/scala/com/daromi/cascade/core/math/Primes.scala
+++ src/core/src/main/scala/com/daromi/cascade/core/math/Primes.scala
@@ -5,12 +5,14 @@ import scala.annotation.tailrec
 def isPrime1(n: Int): Boolean =
   require(n > 1, "n must be greater than 1")
 
-  val limit = math.sqrt(n).toInt
+  if n == 2 || n == 3 then true
+  else
+    val limit = math.sqrt(n).toInt
 
-  @tailrec
-  def helper(divisor: Int): Boolean =
-    if divisor > limit then true
-    else if n % divisor == 0 then false
-    else helper(divisor + 1)
+    @tailrec
+    def helper(divisor: Int): Boolean =
+      if divisor > limit then true
+      else if n % divisor == 0 then false
+      else helper(divisor + 1)
 
-  helper(2)
+    helper(2)
