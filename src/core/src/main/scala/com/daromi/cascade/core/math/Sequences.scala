07105ab6a33145e4d0eb3bf30aee4940a2457aa2
daromi94 <davromero07@gmail.com>
2025-01-30 20:28:25 -0500

    refactor: :truck: rename naive sequence functions

--- src/core/src/main/scala/com/daromi/cascade/core/math/Sequences.scala
+++ src/core/src/main/scala/com/daromi/cascade/core/math/Sequences.scala
@@ -1,14 +1,14 @@
 package com.daromi.cascade.core.math
 
-def factorialNaive(n: Int): Int =
+def factorial1(n: Int): Int =
   require(n >= 0, "n must be non-negative")
 
   if n == 0 || n == 1 then 1
-  else n * factorialNaive(n - 1)
+  else n * factorial1(n - 1)
 
-def fibonacciNaive(n: Int): Int =
+def fibonacci1(n: Int): Int =
   require(n >= 0, "n must be non-negative")
 
   if n == 0 then 0
   else if n == 1 then 1
-  else fibonacciNaive(n - 1) + fibonacciNaive(n - 2)
+  else fibonacci1(n - 1) + fibonacci1(n - 2)
