fe1946f5e4e22beaa3603fc36a0ff7a9905fe347
daromi94 <davromero07@gmail.com>
2025-02-13 21:54:37 -0500

    refactor: :recycle: use int for sequence functions

--- src/core/src/main/scala/com/daromi/cascade/core/math/Sequences.scala
+++ src/core/src/main/scala/com/daromi/cascade/core/math/Sequences.scala
@@ -1,14 +1,14 @@
 package com.daromi.cascade.core.math
 
-def factorial1(n: Long): Long =
-  require(n >= 0L, "n must be non-negative")
+def factorial1(n: Int): Long =
+  require(n >= 0, "n must be non-negative")
 
-  if n == 0L || n == 1L then 1L
-  else n * factorial1(n - 1L)
+  if n == 0 || n == 1 then 1L
+  else n * factorial1(n - 1)
 
-def fibonacci1(n: Long): Long =
-  require(n >= 0L, "n must be non-negative")
+def fibonacci1(n: Int): Long =
+  require(n >= 0, "n must be non-negative")
 
-  if n == 0L then 0L
-  else if n == 1L then 1L
-  else fibonacci1(n - 1L) + fibonacci1(n - 2L)
+  if n == 0 then 0L
+  else if n == 1 then 1L
+  else fibonacci1(n - 1) + fibonacci1(n - 2)
--- src/core/src/main/scala/com/daromi/cascade/core/math/Series.scala
+++ src/core/src/main/scala/com/daromi/cascade/core/math/Series.scala
@@ -1,12 +1,12 @@
 package com.daromi.cascade.core.math
 
-def triangular(n: Long): Long =
-  require(n >= 1L, "n must be positive")
+def triangular(n: Int): Int =
+  require(n >= 1, "n must be positive")
 
-  n * (n + 1L) / 2L
+  n * (n + 1) / 2
 
-def triangular1(n: Long): Long =
-  require(n >= 1L, "n must be positive")
+def triangular1(n: Int): Int =
+  require(n >= 1, "n must be positive")
 
-  if (n == 1L) 1L
-  else n + triangular(n - 1L)
+  if (n == 1) 1
+  else n + triangular(n - 1)
